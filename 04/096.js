8a8aef0014d51c6cb1031d176351f6b9213eef4d
z <552436310@qq.com>
2022-06-12 16:06:10 +0800

    6.12

--- 04/096.js
+++ 04/096.js
@@ -5,11 +5,10 @@
  * @return {boolean}
  */
  var isInterleave = function(s1, s2, s3) {
-    if (!s1 && !s2 && !s3) return true
     let len1 = s1.length, len2 = s2.length
     if (len1 + len2 !== s3.length) return false
     let dp  = new Array(len1 + 1).fill(0).map(() => new Array(len2 + 1).fill(false))
-    dp[0][0] = s1.length || s2.length ? true : false
+    dp[0][0] = true
     for (let i = 1; i <= len1; i++) {
         if (dp[i-1][0] && s1[i-1] === s3[i-1]) dp[i][0] = true
     }
