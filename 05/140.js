2c5e197dfaee5cd193e4c0b52813ae3a150c2a38
z <552436310@qq.com>
2022-07-02 16:13:27 +0800

    7.2

--- 05/140.js
+++ 05/140.js
@@ -89,13 +89,58 @@ function MySort(arr) {
         FastSort(arr, left + 1, j)
     }
 
-    // 直接选择排序
-    // 时间复杂度：O()
-    // 空间复杂度：O()
+    // 直接选择排序 不稳定
+    // 时间复杂度：O(n2)
+    // 空间复杂度：O(1)
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = i
+        for (let j = i + 1; j < arr.length; j++) {
+            min = arr[j] < arr[min] ? j : min
+        }
+        [arr[i], arr[min]] = [arr[min], arr[i]]
+    }
+    return arr
 
-    // 堆排序
-    // 时间复杂度：O()
-    // 空间复杂度：O()
+    // 堆排序 不稳定
+    // 时间复杂度：O(nlogn)
+    // 空间复杂度：O(n) / O(1)
+    function Heap() {
+        this.queue = []
+    }
+    Heap.prototype.push = function(entry) {
+        this.queue.push(entry)
+        let index = this.queue.length - 1
+        let parent = Math.floor((index - 1) / 2)
+        while (parent >= 0 && this.queue[parent] > this.queue[index]) {
+            [this.queue[parent], this.queue[index]] = [this.queue[index], this.queue[parent]]
+            index = parent
+            parent = Math.floor((index - 1) / 2)
+        }
+    }
+    Heap.prototype.pop = function() {
+        if (this.queue.length === 1) return this.queue.pop()
+        let res = this.queue[0]
+        this.queue[0] = this.queue.pop()
+        let index = 0, left = 1, child
+        if (this.queue.length < 3) child = left
+        else child = this.queue[1] > this.queue[2] ? 2 : 1
+        while (this.queue[child] && this.queue[index] > this.queue[child]) {
+            [this.queue[child], this.queue[index]] = [this.queue[index], this.queue[child]]
+            index = child
+            left = 2 * index + 1
+            if (this.queue.length < left + 2) child = left
+            else child = this.queue[left] > this.queue[left + 1] ? left + 1 : left
+        }
+        return res
+    }
+    Heap.prototype.size = function() {
+        return this.queue.length
+    }
+    
+    let heap = new Heap(), res = []
+    for (let i of arr) heap.push(i)
+    while (heap.size()) res.push(heap.pop())
+    return res
 
     // 归并排序
     // 时间复杂度：O()
